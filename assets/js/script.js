332e32dda2602ab9286d9a4605988ad5b7ec477a
rmac0411 <rmac0411@gmail.com>
2022-04-28 20:59:54 -0700

    pretty much done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,9 @@
 // TODO: Declare any global variables we need
+let total = 0
+let heads = 0
+let tails = 0
+let headsPercent = 0
+let tailsPercent = 0
 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -7,6 +12,39 @@ document.addEventListener('DOMContentLoaded', function () {
     console.log('Hi')
 
     // TODO: Add event listener and handler for flip and clear buttons
+    let flip = document.querySelector("#flip")
+    let image = document.querySelector("img")
+    let displayHeads = document.querySelector("#heads")
+    let displayTails = document.querySelector("#tails")
+    let displayHeadsPercent = document.querySelector("#heads-percent")
+    let displayTailsPercent = document.querySelector("#tails-percent")
+    flip.addEventListener("click", function(){
+       let val = Math.random();
+        if (val > .5)
+        {        
+            image.setAttribute("src", "assets/images/penny-heads.jpg")
+            heads = heads +1
+            displayHeads.textContent = heads
+            
+        } else {
+            image.setAttribute("src", "assets/images/penny-tails.jpg")
+            tails = tails + 1
+            displayTails.textContent = tails
+            
+
+
+        }
+        total = heads + tails
+        if (total > 0){
+            headsPercent = (heads / total) * 100
+            
+            tailsPercent = (tails / total) *100
+
+        }
+
+    })
+
+
 
     // Flip Button Click Handler
         // TODO: Determine flip outcome
